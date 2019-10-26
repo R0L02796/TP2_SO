b38e472cd38fd0f505497be6f617fabc6bd2f4ab
R0L02796 <48720104+R0L02796@users.noreply.github.com>
2019-10-26 20:59:52 -0300

    Update memoryManager.c

--- Kernel/memoryManager.c
+++ Kernel/memoryManager.c
@@ -372,7 +372,7 @@ page * getOptimalPage(size_t space)
        newLine();
         char buffer20[10];
         putStr("free pages\t");
-        putStr(decToStr(memory->freePagesLv[optLv-MIN_LEVEL], buffer20));
+        putStr(decToStr(optLv, buffer20));
         newLine();
     if (memory->freePagesLv[optLv-MIN_LEVEL] == 0 && optLv >= memory->minLv)
     {
